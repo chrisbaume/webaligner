49d865625d70cf5cc6c35998152855a82afde173
Chris Baume <chris.baume@bbc.co.uk>
2017-07-21 14:50:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# WebAligner
+
+A client-side forced aligner for speech that attempts to find word boundaries. Works by plotting the RMS energy of the speech, then using gradient descent to find the local minimum for word boundaries.
+
+## Usage
+
+```
+bower install webaligner
+```
+
+```
+<script src="bower_components/webaligner/webaligner.js"></script>
+<script>
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var audioData = new ArrayBuffer(2000);
+  var startTime = 2.3;
+  var endTime = 5.6;
+  var words = "the quick brown fox jumps over the lazy dog";
+  var weightByWordLength = true;
+  var useTimes = function(times) {
+    // do something with the timestamps
+  }
+  webAligner.align(audioCtx, audioData, startTime, endTime,
+                     words, weightByWordLength, useTimes);
+</script>
+```
---
+++ webaligner.js
@@ -0,0 +1,90 @@
+var webAligner = (function()
+{
+  var exports = {};
+
+  function descend(array, startPos)
+  {
+    var pos = startPos;
+    var min = array[pos];
+    while (true) {
+      var bottom = true;
+      var newPos = pos;
+      if (pos>0) {
+        if (array[pos-1] < min) {
+          min=array[pos-1];
+          newPos--;
+          bottom = false;
+        }
+      }
+      if (pos<array.length-1) {
+        if (array[pos+1] < min) {
+          min = array[pos+1];
+          newPos++;
+          bottom = false;
+        }
+      }
+      pos = newPos;
+      if (bottom) break;
+    }
+    return pos;
+  }
+
+  exports.align = function(audioCtx, audioData, startTs, endTs, words, weight, cb)
+  {
+    var blockLen = 0.025; // 100ms block size
+    var stepLen = 0.01; // 20ms step size
+    var duration = endTs-startTs;
+    var startStep = Math.floor((startTs-(blockLen/2))/stepLen);
+    var endStep = Math.floor((endTs-(blockLen/2))/stepLen);
+    var steps = endStep-startStep;
+    var blockSize = Math.floor(blockLen*audioCtx.sampleRate);
+    var stepSize = Math.floor(stepLen*audioCtx.sampleRate);
+
+    wordArray = words.split(" ");
+    var characters = words.length-wordArray.length+1;
+    if (wordArray.length<2) return console.log("Need at least two words!");
+    wordStepPos = [];
+    if (weight) {
+      console.log('Weighting by word length');
+      var position = 0;
+      for (i=0; i<wordArray.length-1; i++) {
+        position += wordArray[i].length;
+        wordStepPos.push(Math.floor(position/characters*steps));
+      }
+    } else {
+      console.log('Not weighting by word length');
+      for (i=1; i<wordArray.length; i++) {
+        wordStepPos.push(Math.floor(i/wordArray.length*steps));
+      }
+    }
+
+    // draw energy
+    audioCtx.decodeAudioData(audioData, function(decodedData)
+    {
+      var audio = decodedData.getChannelData(0);
+      var totalDuration = decodedData.duration;
+      if (endTs>totalDuration) return console.log('End time after end of file');
+      var rms = [];
+
+      for (stepNum = startStep; stepNum < endStep; stepNum++) {
+        var blockTotal = 0.0;
+        for (i=0; i<blockSize; i++) {
+          var sample = audio[(stepSize*stepNum)+i];
+          blockTotal += sample*sample;
+        }
+        rms.push(Math.sqrt(blockTotal/blockSize));
+      }
+      console.log(rms);
+
+      var times = [];
+      times.push(startTs);
+      for (i=0; i<wordStepPos.length; i++) {
+        wordStepPos[i] = descend(rms, wordStepPos[i]);
+        times.push(startTs + (wordStepPos[i]/steps*duration));
+      }
+      times.push(endTs);
+      cb(times);
+    });
+  }
+  return exports;
+}());
